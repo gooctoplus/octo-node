2ecb3fd869a6f4af6b60c6fff6e0b6456c7ce93b
awesomearpit <jarpit1298@gmail.com>
2024-04-30 19:43:57 +0530

    feat: add, update, get and get by id issues api

---
+++ backend/models/issueModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const issueSchema = new mongoose.Schema({
+  issueId: { type: String, required: true, unique: true },
+  orgId: { type: String, required: true },
+  project: {
+    name: { type: String, required: true },
+    projectId: { type: String, required: true },
+    key: { type: String, required: true },
+  },
+  issueType: { type: String, required: true },
+  priority: { type: String, required: true },
+  status: { type: String, required: true },
+  summary: { type: String, required: true },
+  reporter: { type: String, required: true },
+  createdAt: { type: String, required: true },
+  lastUpdatedAt: { type: String, required: true },
+});
+
+const issueModel = mongoose.model("Issue", issueSchema);
+
+export default issueModel;
