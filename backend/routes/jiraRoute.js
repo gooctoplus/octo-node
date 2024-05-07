2071b95c5a7114d6538d06773262c6775b06ccb4
Abhishek Gupta <webtaculars.ai@gmail.com>
2024-05-07 10:04:34 +0530

    move python path to env

--- backend/routes/jiraRoute.js
+++ backend/routes/jiraRoute.js
@@ -3,6 +3,7 @@ import { commentOnTicket } from '../services/jira/index.js';
 import { spawn } from 'child_process';
 import Org from '../models/orgModel';
 import Project from '../models/projectModel';
+import config from "../config";
 
 const router = express.Router();
 router.post('/:orgId/webhook', async (req, res) => {
@@ -11,12 +12,12 @@ router.post('/:orgId/webhook', async (req, res) => {
     const { orgId } = req.params;
     console.log(issue)
 
-    const org = await Org.findOne({orgId});
-    if(org) {
-      const {pineconeAPIKey, openAIKey } = org
-      const projects = await Project.find({orgId});
+    const org = await Org.findOne({ orgId });
+    if (org) {
+      const { pineconeAPIKey, openAIKey } = org
+      const projects = await Project.find({ orgId });
       const currentProject = projects.find(project => issue.self.includes(project.url));
-      
+
       const repoUrl = currentProject.repos[0].url
       const repoTargetPath = currentProject.repos[0].repoTargetPath;
       const pineconeIndex = currentProject.repos[0].pineconeIndex;
@@ -26,48 +27,50 @@ router.post('/:orgId/webhook', async (req, res) => {
       const projectUrl = currentProject.url
 
       if (req.body.webhookEvent === 'comment_created' && req.body.comment.author.displayName === "Octo") {
-          console.log('Update made by the bot or author is Octo, ignoring to prevent loop.');
-          return res.status(200).send('Ignored sdbot update');
+        console.log('Update made by the bot or author is Octo, ignoring to prevent loop.');
+        return res.status(200).send('Ignored sdbot update');
       }
-    
+
       if (req.body.webhookEvent === "jira:issue_updated" && req.body.issue_event_type_name === "issue_assigned" && req.body.issue.fields.assignee.displayName === "Octo") {
-    
-          const ticketId = issue.key;
-          const ticketDescription = issue.fields.description
-          
-          // Log the assigned ticket
-          console.log(`Assigned ticket: ${ticketId}`);
-    
-          const pythonProcess = spawn('/app/octoplus/bin/python', [
-            '/app/octoplus/octo/main.py', 
-            `--ticket=${ticketDescription}`, 
-            `--ticket-id=${ticketId}`, 
-            `--pineconeAPIKey=${pineconeAPIKey}`, 
-            `--openAIKey=${openAIKey}`, 
-            `--pineconeIndex=${pineconeIndex}`, 
-            `--repoUrl=${repoUrl}`, 
-            `--repoTargetPath=${repoTargetPath}`,
-            `--defaultBranch=${defaultBranch}`,
-            `--languages=${languages.join(',')}`,
-            `--suffixes=${suffixes.join(',')}`,
-            `--projectUrl=${projectUrl}`,
-          ], {
-            cwd: '/app/octoplus/octo/'
-          });
-          pythonProcess.stdout.on('data', (data) => {
-              console.log(`stdout: ${data}`);
-          });
-    
-          pythonProcess.stderr.on('data', (data) => {
-              console.error(`stderr: ${data}`);
-          });
-    
-          pythonProcess.on('close', (code) => {
-              console.log(`child process exited with code ${code}`);
-              res.send('Python script executed');
-          });
-    
-      } 
+
+        const ticketId = issue.key;
+        const ticketDescription = issue.fields.description
+
+        // Log the assigned ticket
+        console.log(`Assigned ticket: ${ticketId}`);
+        const pythonPath = config.PYTHON_PATH || '/app/octoplus/bin/python';
+        const projectPath = config.PROJECT_PATH || '/app/octoplus/octo/main.py';
+        const cwd = config.CURRENT_WORKING_DIRECTORY || '/app/octoplus/octo/';
+        const pythonProcess = spawn(pythonPath, [
+          projectPath,
+          `--ticket=${ticketDescription}`,
+          `--ticket-id=${ticketId}`,
+          `--pineconeAPIKey=${pineconeAPIKey}`,
+          `--openAIKey=${openAIKey}`,
+          `--pineconeIndex=${pineconeIndex}`,
+          `--repoUrl=${repoUrl}`,
+          `--repoTargetPath=${repoTargetPath}`,
+          `--defaultBranch=${defaultBranch}`,
+          `--languages=${languages.join(',')}`,
+          `--suffixes=${suffixes.join(',')}`,
+          `--projectUrl=${projectUrl}`,
+        ], {
+          cwd: cwd
+        });
+        pythonProcess.stdout.on('data', (data) => {
+          console.log(`stdout: ${data}`);
+        });
+
+        pythonProcess.stderr.on('data', (data) => {
+          console.error(`stderr: ${data}`);
+        });
+
+        pythonProcess.on('close', (code) => {
+          console.log(`child process exited with code ${code}`);
+          res.send('Python script executed');
+        });
+
+      }
     } else {
       console.log('org not found')
       res.status(404).send('Org not found');
@@ -79,9 +82,9 @@ router.post('/:orgId/webhook', async (req, res) => {
   }
 });
 
-router.post('/comment', async (req, res)  =>  {
+router.post('/comment', async (req, res) => {
   const { comment, ticketId, status, prUrl, projectUrl } = req.body;
-  const project = await Project.findOne({url: projectUrl});
+  const project = await Project.findOne({ url: projectUrl });
 
   commentOnTicket(project.url, project.jiraToken, project.jiraEmail, ticketId, comment, status, prUrl)
   res.send('Comment received successfully');
